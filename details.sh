4d94b9ec513273857108072294de323c7193ed8e
testuser1 <testuser1@ip-10-1-1-86.ec2.internal>
2019-03-26 02:21:29 +0000

    Added sudo code and list users in the machine

--- details.sh
+++ details.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "THIS SCRIPT WILL PROVIDE INFORMATION ABOUT THE LOCAL SYSTEM"
+echo
+#GIVES HOSTNAME
 echo $(hostname)
+echo
+#GIVES USER EXECUTED THIS SCRIPT
 echo $(whoami)
-echo $(id -un)
+echo
+#GIVES USERS IN THE SYSTEM
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
